1527a2dd6e2dcaf39b4ab86860d85322ce5cad72
OrielGer <orielger@gmail.com>
2025-10-22 15:48:38 +0300

    Add files via upload

---
+++ c2_client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+C2 Client Launcher
+Quick-start script for C2 client
+
+IMPORTANT: Edit client/main.py to change SERVER_HOST to your server's IP
+"""
+
+if __name__ == '__main__':
+    from client.main import main
+    main()
