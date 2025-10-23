e8001b3f790dce61268ca786b6feb50755898ad2
OrielGer <orielger@gmail.com>
2025-10-23 13:22:57 +0300

    Delete common directory

--- common/exceptions.py
+++
@@ -1,9 +0,0 @@
-"""
-Custom Exceptions Module
-Defines C2-specific exceptions for better error handling
-"""
-
-
-class MessageTooLargeError(Exception):
-    """Raised when message exceeds size limit"""
-    pass
